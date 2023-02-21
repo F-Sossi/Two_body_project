3f0bbe7e85ee5efbfd3f2bb99c1ef14ebd121efb
Frank <frank.sossi@gmail.com>
2023-02-20 22:56:49 -0800

    added openGL (makes a triangle at this point) added cmake support and a skeleton for the project

---
+++ build/CMakeFiles/my_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_project.dir/src/my_project_generated_kernel.cu.o"
+  "my_project"
+  "my_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/my_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
