8734196847402db6f62801b7deca506191471cbb
Frank <frank.sossi@gmail.com>
2023-02-25 22:28:54 -0800

    init

---
+++ build/CMakeFiles/communicate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/communicate.dir/src/communicate_generated_communicate.cu.o"
+  "libcommunicate.a"
+  "libcommunicate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/communicate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
